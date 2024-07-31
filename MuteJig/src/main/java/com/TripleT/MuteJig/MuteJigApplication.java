e00139a5ddc21923c74b69ade309b1e802ddfe20
Zjn <129529039+zjn1404@users.noreply.github.com>
2024-07-31 21:35:58 +0700

    Some update: . Add github repo to file doc . Add spring boot folder project

---
+++ MuteJig/src/main/java/com/TripleT/MuteJig/MuteJigApplication.java
@@ -0,0 +1,13 @@
+package com.TripleT.MuteJig;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MuteJigApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MuteJigApplication.class, args);
+	}
+
+}
---
+++ MuteJig/src/test/java/com/TripleT/MuteJig/MuteJigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.TripleT.MuteJig;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MuteJigApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
